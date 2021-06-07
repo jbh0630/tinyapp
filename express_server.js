e11c665d01666c556fda1d7db5e4c53fb0278289
jbh0630 <jayjo0630@gmail.com>
2021-06-07 23:44:18 +0000

    URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+const generateRandomString = function() {
+  const charaters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( let i = 0; i < charaters.length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+    charaters.length));
+ }
+ return result;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -32,7 +43,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok");        
+});
+
