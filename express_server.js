238f6bbf2864fecfe2c2c583d4ec4c730cd2a8c0
jbh0630 <jayjo0630@gmail.com>
2021-06-08 22:09:05 +0000

    added Updating feature

--- express_server.js
+++ express_server.js
@@ -47,19 +47,25 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); 
-  res.send("Ok");        
-});
-
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok");        
+});
+
 app.post('/urls/:shortURL/delete', (req, res) => {
   const toDelete = req.params.shortURL;
   delete urlDatabase[toDelete];
+  res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL/edit', (req, res) => {
+  const toEdit = req.params.shortURL;
+  urlDatabase[toEdit] = req.body.url;
   res.redirect('/urls');
 });
 
