52fc9284ab4010f381d1d04169247c570deb2c6b
jbh0630 <jayjo0630@gmail.com>
2021-06-09 22:47:17 +0000

    login and register features and event hanler

--- express_server.js
+++ express_server.js
@@ -3,18 +3,34 @@ const app = express();
 const PORT = 8080;
 const cookieParser = require('cookie-parser')
 const { v4: uuidv4 } = require('uuid');
-
+const morgan = require('morgan');
 
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
-
+app.use(morgan('dev'));
 app.set("view engine", "ejs");
+
+const generateId = () => {
+  return uuidv4().split('-')[1];
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "1234"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "123"
+  }
+}
 
 app.get("/", (req, res) => {
   res.send('Hello!');
@@ -30,45 +46,53 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    userId: req.cookies["userId"],
     urls: urlDatabase 
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const username = req.cookies["username"];
-  res.render("urls_new", { username });
-});
-
-app.get('/urls/error', (req, res) => {
-  res.send('<h1>You should login first!</h1>');
+  const userId = req.cookies["userId"];
+  if (!userId) {
+    return res.status(401).send('<h1>You must login first!</h1>');
+  }
+  res.render("urls_new", { userId });
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"] };
+    userId: req.cookies["userId"] };
+
+    if (!req.cookies["userId"]) {
+      return res.status(401).send('<h1>You must login first!</h1>');
+    }
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const vars = {
-    username: req.cookies["username"],
-    longURL: urlDatabase[req.params.shortURL]
-  };
-  res.render('urls_show', vars);
+app.get('/register', (req, res) => {
+  const userId = req.cookies["userId"];
+  res.render('register', { userId });
+});
+
+app.get('/login', (req, res) => {
+  const userId = req.cookies["userId"];
+  res.render('login', { userId });
 });
 
 app.post("/urls", (req, res) => {
   const newLongURL = req.body.longURL;
-  const newShortURL = uuidv4().split('-')[1];
+  const newShortURL = generateId();
   urlDatabase[newShortURL] = newLongURL;
   res.redirect('/urls');
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
+  if (!req.cookies["userId"]) {
+    return res.status(401).send('<h1>You must login first!</h1>');
+  }
   const toDelete = req.params.shortURL;
   delete urlDatabase[toDelete];
   res.redirect('/urls');
@@ -80,20 +104,61 @@ app.post('/urls/:shortURL/edit', (req, res) => {
   res.redirect('/urls');
 });
 
-app.post('/urls/login', (req, res) => {
-  const username = req.body.username;
-  res.cookie('username', username);
+app.post('/logout', (req, res) => {
+  const userId = req.cookies["userId"];
+  res.clearCookie('userId', userId);
   res.redirect('/urls');
 });
 
-app.post('/urls/logout', (req, res) => {
-  const username = req.body.username;
-  res.clearCookie('username', username);
-  res.redirect('/urls');
+app.post('/register', (req, res) => {
+  console.log(req.body.email);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      return res.status(401).send('Email already exist');
+    } else if (!email || !password) {
+      return res.status(401).send('You must enter an email AND a password');
+    }
+  }
+  
+  const newUserId = generateId();
+  const newUser = {
+    id: newUserId,
+    email,
+    password
+  };
+
+  users[newUserId] = newUser;
+  res.redirect('/login');
 });
 
-app.post('/urls/error', (req, res) => {
-  res.send('<h1>You should login first!<h1>');
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let foundUser;
+
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      foundUser = user;
+    }
+  }
+
+  if (!foundUser) {
+    return res.status(401).send('You entered invalid Email or Password! Try Again!');
+  }
+
+  if (foundUser.password !== password) {
+    return res.status(401).send('Incorrect Password. Try Again!');
+  }
+
+  res.cookie('userId', foundUser.id);
+  res.redirect('/urls');
+
 });
 
 app.listen(PORT, () => {
