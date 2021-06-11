30a56d5fe1e014d279e5bdbdeb7522ccb0336050
jbh0630 <jayjo0630@gmail.com>
2021-06-11 21:32:59 +0000

    final error fixed

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
+
 const getUserByEmail = function(email, database) {
   let user;
   for (const data in database) {
@@ -8,4 +10,22 @@ const getUserByEmail = function(email, database) {
   return user;
 };
 
-module.exports = { getUserByEmail };
+const urlsForUser = function(id, urlDatabase, users) {
+  const foundURL = {};
+  let userEmail;
+  for (const url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      foundURL[url] = { 
+        longURL: urlDatabase[url].longURL
+      };
+    };
+  }
+  userEmail = users[id].email;
+  return { foundURL, email: userEmail };
+};
+
+const generateId = () => {
+  return uuidv4().split('-')[1];
+};
+
+module.exports = { getUserByEmail, urlsForUser, generateId };
